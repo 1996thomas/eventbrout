a8c07d44908956f08932836b392177109d6c26eb
Thomas <thomas.kanthack@gmail.com>
2023-02-20 14:31:33 +0100

    add mailer-adress-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  get 'pagetests/index'
+  root 'pagetests/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

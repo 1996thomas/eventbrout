a1f27e4c7dbb0e7dd4b89ab0fe0b43c96b62aea5
Thomas <thomas.kanthack@gmail.com>
2023-02-20 14:04:10 +0100

    add mailer-adress

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'no-reply@eventbrout.fr'
+  default from: 'thomas.kanthack@gmail.com'
 
   def welcome_email(user)
     @user = user
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   get 'pagetests/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

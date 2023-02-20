ec045936b7ec89432152e9779d414fb94c9c0c07
Thomas <thomas.kanthack@gmail.com>
2023-02-20 13:51:55 +0100

    add mailer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.delete_all
+Event.delete_all
+
+
+20.times do 
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4), email: "test@yopmail.com")
+end
+
+duration_time = [5,10,15,20,30,60]
+price = [10,20,30]
+
+10.times do 
+  Event.create(start_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'), duration: duration_time[rand(0..5)], price: price[rand(0..2)], location: Faker::Address.city, description: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4), title:Faker::Music::RockBand.name )
+end
+
+users = User.all
+events = Event.all
+
+10.times do 
+  Attendance.create(strip_customer_id: Faker::IDNumber.valid, user_id: users[rand(0..19)].id, event_id: events[rand(0..9)].id)
+end

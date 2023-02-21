76f3a31986591bfc963c7d4f46360e42ee5be589
Thomas <thomas.kanthack@gmail.com>
2023-02-21 16:46:42 +0100

    test email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,4 @@
 class UsersController < ApplicationController
   def show
   end
-
-  def index
-  end
-
-  def edit
-  end
-
-  def update
-  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
 require_relative "application"
 # config.action_mailer.delivery_method = :letter_opener_web
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['MAILJET_LOGIN'],
+  :password => ENV['MAILJET_PWD'],
+  :domain => 'https://eventbrout.herokuapp.com/',
+  :address => 'in-v3.mailjet.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!

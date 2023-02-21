240873b17c34c775fb259260b9b92e1d1e02c60c
Thomas <thomas.kanthack@gmail.com>
2023-02-21 16:54:31 +0100

    test email_2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  after_create :welcome_send
+  # after_create :welcome_send
   has_many :attendances
   has_many :events, through: :attendances
   
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
+  # def welcome_send
+  #   UserMailer.welcome_email(self).deliver_now
+  # end
 end

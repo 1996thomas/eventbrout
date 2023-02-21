eaf90cd9350cabb9bd832162bf5dde498315ce84
Thomas <thomas.kanthack@gmail.com>
2023-02-21 17:23:52 +0100

    ajout des events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,13 +13,13 @@ class EventsController < ApplicationController
 
   def create 
     @event = Event.new(event_params)
-    @event.update(admin: current_user)
-    @event.save
+    @event.update!(admin: current_user)
+    @event.save!
     redirect_to events_path
   end
 
   private
   def event_params 
-    params.require(:event).permit(:start_date,:title,:description,:price,:location,:duration)
+    params.require(:event).permit(:start_date,:title,:description,:price,:location,:duration,:admin_id)
   end
 end

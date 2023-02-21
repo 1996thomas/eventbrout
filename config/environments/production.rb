040f0db0b84146f5e36d08c99f0836797cbb1dc3
Thomas <thomas.kanthack@gmail.com>
2023-02-21 16:58:09 +0100

    test email_3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "eventbrout_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'eventbrout.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

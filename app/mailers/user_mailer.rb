1a9aa896abf3a8a97daf14038ab47e09a313f117
Thomas <thomas.kanthack@gmail.com>
2023-02-21 16:52:03 +0100

    test email_2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'thomas.kanthack@gmail.com'
+  default from: 'thomas_test@yopmail.com'
 
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
